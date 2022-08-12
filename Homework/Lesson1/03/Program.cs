4308742da8fa4e9c4d1b85f8e006f3bbaae17fd1
Vitali <v.podskrebkin@gmail.com>
2022-08-12 18:55:39 +0300

    Added ex03

---
+++ Homework/Lesson1/03/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет*/
+
+int a = 4;
+int b = -3;
+int c = 7;
+if (a % 2 ==0)
+{
+    Console.WriteLine(a + " " + "Да");
+}    
+else
+{
+    Console.WriteLine("Нет");
+}
+if (b % 2 ==0)
+{
+    Console.WriteLine(b + "Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+if (c % 2 ==0)
+{
+    Console.WriteLine(c + "Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
