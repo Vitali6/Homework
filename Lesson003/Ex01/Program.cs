cca94b59cc87b588b16c2378f09f20b792600147
Vitali <v.podskrebkin@gmail.com>
2022-08-13 18:16:27 +0300

    Added ex02_02

---
+++ Lesson003/Ex01/Program.cs
@@ -0,0 +1,11 @@
+﻿/*Напишите программу, которая принимает на вход координаты точки (X и Y), 
+причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.*/
+
+int y = Convert.ToInt32(Console.Readline());
+int x = Convert.ToInt32(Console.Readline());
+if (x < 0 && y < 0){
+    Concole.WriteLine("Точка в плоскости 4");
+}else if (x>0 && y<0){
+    Console.WriteLine("Точка в плоскости 3");
+}
+
