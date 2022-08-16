bc6736d406134e67dfb84305eb818ae1a2374c4b
Vitali <v.podskrebkin@gmail.com>
2022-08-16 20:46:10 +0300

    Added ex03_03

---
+++ Homework/Lesson03/Ex03/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125*/
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int n = 1;
+
+while (n <= a)
+{
+    Console.WriteLine(Math.Pow(n, 3));
+    n++;
+} 
+
