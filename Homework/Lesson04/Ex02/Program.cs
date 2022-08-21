4db4aa623c9a11f6c4e98dc3e2cec320343e26e2
Vitali <v.podskrebkin@gmail.com>
2022-08-21 13:54:24 +0300

    Added ex04_02

---
+++ Homework/Lesson04/Ex02/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+Console.WriteLine("Введите число: ");
+int i = Convert.ToInt32(Console.ReadLine());
+int value = 999;
+for (int a = 0; a <= value; a++)
+{
+    Console.WriteLine($"  ");
+}    
