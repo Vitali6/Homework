8403d0435f1f9e4ed8f845bbfcf0ffc001d50b6f
Vitali <v.podskrebkin@gmail.com>
2022-08-26 18:33:13 +0300

    Added Homework05_Ex03

--- Homework/Lesson05/Ex03/Program.cs
+++ Homework/Lesson05/Ex03/Program.cs
@@ -11,5 +11,23 @@ void FillArray(double[]collection)
         collection[i] = new Random().Next(-10, 10) + Math.Round(new Random().NextDouble() , 2);
         Console.Write($"{collection[i]}; ");
     }
+    Console.WriteLine();
 }
-FillArray(newarray);
+void Different(double[] arr)
+{
+    double min = arr[0];
+    double max = arr[0];
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (arr[i] > max) max = arr[i];
+        if (arr[i] < min) min = arr[i];
+    }
+    Console.WriteLine(max - min);
+} 
+Console.Clear();
+Console.WriteLine("Введите длинну массива: ");
+int N = Convert.ToInt32(Console.ReadLine());
+double[] mass = new double[N];
+FillArray(mass);
+Console.WriteLine("Разница между max и min: ");
+Different(mass);
