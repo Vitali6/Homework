1d0810c9fcd20eeadc71e5f6524dd28b4681fea3
Vitali <v.podskrebkin@gmail.com>
2022-08-26 18:12:53 +0300

    Added Homework05_Ex02

--- Homework/Lesson05/Ex02/Program.cs
+++ Homework/Lesson05/Ex02/Program.cs
@@ -2,3 +2,27 @@
 [3, 7, 23, 12] -> 19
 [-4, -6, 89, 6] -> 0*/
 
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 99);
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+void ClearArray(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i % 2 !=0) sum += arr[i];
+    }
+    Console.WriteLine(sum);
+}
+Console.Clear();
+Console.WriteLine("Введите длинну массива: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] mass = new int[N];
+FillArray(mass);
+ClearArray(mass);
