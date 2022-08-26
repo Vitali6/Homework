7d7a5d8b5d90cd476479274a1a6d552c02136e99
Vitali <v.podskrebkin@gmail.com>
2022-08-26 17:56:54 +0300

    Added Homework05_Ex01

---
+++ Homework/Lesson05/Ex01/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2*/
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+        Console.Write($"{array[i]} ");
+    }
+}
+void ClearArray(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 ==0) count++;                 
+    }
+    Console.Write($"Четных чисел в массиве = {count}");
+}
+Console.Clear();
+Console.Write("Введите длинну массива: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] mass = new int[N];
+FillArray(mass);
+Console.WriteLine();
+ClearArray(mass);
---
+++ Homework/Lesson05/Ex03/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+[3 7 22 2 78] -> 76*/
+
+Console.Clear();
+
+double[] newarray = new double[20];
+void FillArray(double[]collection)
+{
+    for (int i = 0; i < collection.Length; i++)
+    {
+        collection[i] = new Random().Next(-10, 10) + Math.Round(new Random().NextDouble() , 2);
+        Console.Write($"{collection[i]}; ");
+    }
+}
+FillArray(newarray);
