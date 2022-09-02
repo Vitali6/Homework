e78ffdb1087803f85086b214da4989049e437c22
Vitali <v.podskrebkin@gmail.com>
2022-09-02 19:37:53 +0300

    Added Homework07

---
+++ Homework/Lesson07/Ex01/Program.cs
@@ -0,0 +1,35 @@
+﻿/*Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9*/
+
+void FillMatrix(double[,] matrix)                                            
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            int mult = new Random().Next(-100, 100);
+            matrix[i, j] = Math.Round(new Random().NextDouble() * mult, 2);
+        }
+    }
+}
+
+void PrintMatrix(double[,] matrix)                                              
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"[{matrix[i, j]}], ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int m = new Random().Next(2, 11);
+int n = new Random().Next(2, 11);
+double[,] numbers = new double[m, n];
+FillMatrix(numbers);
+PrintMatrix(numbers);
---
+++ Homework/Lesson07/Ex02/Program.cs
@@ -0,0 +1,70 @@
+﻿/*Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+1, 7 -> такого числа в массиве нет*/
+
+Console.Write("Введите количество строк: ");
+int m = 0;
+int.TryParse(Console.ReadLine(), out m);
+
+Console.Write("Введите количество столбцов: ");
+int n = 0;
+int.TryParse(Console.ReadLine(), out n);
+
+int[,] array = new int[m, n];
+Console.WriteLine();
+
+void NewArray(int[,] array)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = new Random().Next(20);
+        }
+    }
+    return;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            Console.Write(string.Format("{0,3} ", array[i, j] + ""));
+        }
+        Console.WriteLine();
+    }
+    return;
+}
+
+void FindNumber(int[,] array)
+{
+    Console.Write("Введите индекс строки: ");
+    int findM = 0;
+    int.TryParse(Console.ReadLine(), out findM);
+
+    Console.Write("Введите индекс столбца: ");
+    int findN = 0;
+    int.TryParse(Console.ReadLine(), out findN);
+
+    if (findM < m && findN < n)
+    {
+        Console.WriteLine($"Значение в индексе {findM},{findN} = {array[findM, findN]}");
+    }
+    else
+    {
+        Console.WriteLine($"Такого числа в массиве нет!");
+    }
+    return;
+}
+
+NewArray(array);
+PrintArray(array);
+Console.WriteLine();
+FindNumber(array);
